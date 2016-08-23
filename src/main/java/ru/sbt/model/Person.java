06f6360e730cf5c3b02c86fa6c06d37761a11d27
Vladimir Papin <vladimir.papin@gmail.com>
2016-08-23 23:55:44 +0300

    override methods hashcode and equals

--- src/main/java/ru/sbt/model/Person.java
+++ src/main/java/ru/sbt/model/Person.java
@@ -4,6 +4,7 @@ public class Person {
     private final String firstName;
     private final String lastName;
     private final int age;
+    private int hashcode = 0;
 
     private Person() {
         throw new IllegalArgumentException("Incorrect object creation \'Person\'. Use \'new Person(firstName, lastName, age);\'");
@@ -34,13 +35,32 @@ public class Person {
 
     @Override
     public int hashCode() {
-        // TODO: 23.08.16
-        return super.hashCode();
+        if (this.hashcode == 0) {
+            int result = 17;
+            result = 37 * result * this.firstName.hashCode();
+            result = 37 * result * this.lastName.hashCode();
+            result = 37 * result * this.age;
+            this.hashcode = result;
+        }
+        return this.hashcode;
     }
 
     @Override
     public boolean equals(Object obj) {
-        // TODO: 23.08.16
-        return super.equals(obj);
+
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Person)) {
+            return false;
+        }
+        Person person = (Person) obj;
+        if ((this.firstName.equals(person.getFirstName())) &&
+                this.lastName.equals(person.getLastName()) &&
+                this.age == person.getAge()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
